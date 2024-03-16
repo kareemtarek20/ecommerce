e7b4f622c7ba1c8392ce6372f92baaf8efc5a996
kareemtarek20 <107767915+kareemtarek20@users.noreply.github.com>
2024-03-16 02:38:35 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+/*=============== SHOW MENU ===============*/
+
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+
+/*=============== SHOW CART ===============*/
+
+
+/*===== CART SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== CART HIDDEN =====*/
+/* Validate if constant exists */
+
+
+/*=============== SHOW LOGIN ===============*/
+
+
+/*===== LOGIN SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== LOGIN HIDDEN =====*/
+/* Validate if constant exists */
+
+
+/*=============== HOME SWIPER ===============*/
+var homeSwiper = new Swiper(".home-swiper", {
+    spaceBetween: 30,
+    loop:'true',
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById("header")
+    //when the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classlist.add("scroll-header"); else header.classlist.remove("scroll-header")
+}
+window.addEventListener("scroll",scrollHeader,);
+/*=============== NEW SWIPER ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== LIGHT BOX ===============*/
+
+
+/*=============== QUESTIONS ACCORDION ===============*/
+
+
+/*=============== STYLE SWITCHER ===============*/
