5c05c4ecb45967837efc51f122b42751d3b80dea
kareemtarek20 <107767915+kareemtarek20@users.noreply.github.com>
2024-03-19 09:25:15 +0200

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,64 +1,84 @@
 /*=============== SHOW MENU ===============*/
 
-
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 
-
 /*=============== SHOW CART ===============*/
-
+const cart = document.getElementById("cart"),
+  cartShop = document.getElementById("cart-shop"),
+  cartClose = document.getElementById("cart-close");
 
 /*===== CART SHOW =====*/
 /* Validate if constant exists */
-
+if (cartShop) {
+  cartShop.addEventListener("click", () => {
+    cart.classList.add("show-cart");
+  });
+}
 
 /*===== CART HIDDEN =====*/
 /* Validate if constant exists */
-
+if (cartClose) {
+  cartClose.addEventListener("click", () => {
+    cart.classList.remove("show-cart");
+  });
+}
 
 /*=============== SHOW LOGIN ===============*/
-
+const login = document.getElementById("login"),
+  LoginButton = document.getElementById("Login-button"),
+  loginClose = document.getElementById("login-close");
 
 /*===== LOGIN SHOW =====*/
 /* Validate if constant exists */
-
+if (LoginButton) {
+  LoginButton.addEventListener("click", () => {
+    login.classList.add("show-login");
+  });
+}
 
 /*===== LOGIN HIDDEN =====*/
 /* Validate if constant exists */
-
+if (loginClose) {
+  loginClose.addEventListener("click", () => {
+    login.classList.remove("show-login");
+  });
+}
 
 /*=============== HOME SWIPER ===============*/
 var homeSwiper = new Swiper(".home-swiper", {
-    spaceBetween: 30,
-    loop:'true',
+  spaceBetween: 30,
+  loop: "true",
 
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-function scrollHeader(){
-    const header = document.getElementById("header")
-    //when the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 50) header.classlist.add("scroll-header"); else header.classlist.remove("scroll-header")
+function scrollHeader() {
+  const header = document.getElementById("header");
+  //when the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 50) header.classlist.add("scroll-header");
+  else header.classlist.remove("scroll-header");
 }
-window.addEventListener("scroll",scrollHeader,);
+window.addEventListener("scroll", scrollHeader);
 /*=============== NEW SWIPER ===============*/
+var newSwiper = new Swiper(".new-swiper", {
+  spaceBetween: 16,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: "true",
+});
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== LIGHT BOX ===============*/
 
-
 /*=============== QUESTIONS ACCORDION ===============*/
-
 
 /*=============== STYLE SWITCHER ===============*/
